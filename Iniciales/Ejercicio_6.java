f95ea8ab49425c8035a44fa6c5304e87f72569a5
AaronUlloa <aaronmars77@gmail.com>
2020-05-13 18:24:38 -0600

    Ejercicios

--- Iniciales/Ejercicio_6.java
+++ Iniciales/Ejercicio_6.java
@@ -6,11 +6,20 @@ public class Ejercicio_6
     {
         Scanner data = new Scanner(System.in);
         int x[] = new int[2];
-        
-        System.out.print("Ingrese L1 y L2: ");
+        int mult = 1, suma = 0;
+
+        //Obtener datos
+        System.out.print("Ingrese L1 y L2 \n");
         for (int i = 0 ; i < x.length ; i++)
         {
+
             x[i] = data.nextInt();
+            suma += x[i];
+            mult *= x[i];
         }
+
+        //Resultado
+        System.out.print("\n Perimetro = "+suma);
+        System.err.print("\n Area = "+mult+"\n");
     }    
 }
