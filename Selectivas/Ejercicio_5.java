913555abd52bad754474638225f006284614ea9c
AaronUlloa <aaronmars77@gmail.com>
2020-05-15 03:14:57 -0600

    Ejercicios finalizados

--- Selectivas/Ejercicio_5.java
+++ Selectivas/Ejercicio_5.java
@@ -1,6 +1,50 @@
 import java.util.Scanner;
 
-public class Ejercicio_5 {
-    Scanner data = new Scanner(System.in);
+
+public class Ejercicio_5 
+{
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+        int codigo = 0;
+        int an = 0;
+        int mes = 0;
+        int an_cad = 0;
     
+    
+        //Pedir Datos
+        do
+        {
+            System.out.print("\n Ingrese codigo del producto: ");
+            codigo = data.nextInt();
+        }
+        while (codigo < 0 || codigo > 150);
+        do
+        {
+            System.out.print("\n Digite el año de elaboracion del producto: ");
+            an = data.nextInt();
+        }
+        while (an < 0);
+        do
+        {
+            System.out.print("\n Ingrese mes de Caducidad: ");
+            mes = data.nextInt();
+        }
+        while (mes < 0);
+        do
+        {
+            System.out.print("\n Digite año de Caducidad: ");
+            an_cad = data.nextInt();
+        }
+        while (an_cad < 0);
+
+        //Logico
+        if ((codigo >= 80 || codigo <= 150) && (an == 2016) && (mes < 9) && (an_cad == 2020))
+        {
+            System.out.print("\n Producto Aceptable, El sistema lo agregara al almacen :) --- \n");
+        }
+        else 
+        {
+            System.out.print("\n Lo sentimos, El sistema no puede asignar un lugar en elalmacén debido a sus características dadas");
+        }
+    }
 }
--- Selectivas/Ejercicio_6.java
+++ Selectivas/Ejercicio_6.java
@@ -1,8 +1,49 @@
 import java.util.Scanner;
 
-public class Ejercicio_6 {
-    public static void main(String[] args) {
+public class Ejercicio_6 
+{
+    public static void main(String[] args) 
+    {
         Scanner data = new Scanner(System.in);
-        
+        char calif = ' ';
+
+        System.out.print("\n Ingrese la calificacion del producto: ");
+        calif = data.nextLine().charAt(0);
+
+
+
+        switch (Character.toUpperCase(calif))
+        {
+            case 'F': System.out.print("\n Frutas \n");
+                      System.out.print("\n Tendra el 20 porciento de descuento todos los Martes \n");
+                      break;
+            case 'V': System.out.print("\n Verduras \n");
+                      System.out.print("\n Tendra el 20 porciento de descuento todos los Martes \n");
+                      break;
+            case 'S': System.out.print("\n Salchichonería \n");
+                      System.out.print("\n Tendra el 15 porciento de descuento todos los fines de semana \n");
+                      break;
+            case 'L': System.out.print("\n Lacteos \n");
+                      break;
+            case 'B': System.out.print("\n Bebes \n");
+                      break;
+            case 'D': System.out.print("\n Damas \n");
+                      break;
+            case 'C': System.out.print("\n Caballero \n");
+                      break;
+            case 'N': System.out.print("\n Niños \n");
+                      break;
+            case 'P': System.out.print("\n Perfumeria \n");
+                      break;
+            case 'M': System.out.print("\n Medicamento Farmaceutico \n");
+                      break;
+            case 'E': System.out.print("\n Electronico \n");
+                      break;
+            case 'H': System.out.print("\n Hogar \n");
+                      break;
+            case 'J': System.out.print("\n Jugeteria \n");
+                      break;
+            default: System.out.print("\n Producto Invalido \n");
+        }
     }
 }
